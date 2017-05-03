aa54d85aaeaa42907573ca0a1b79d411dd738fe6
Cédric Dupont <lunarok@gmail.com>
2017-05-03 12:00:13 +0200

    Update apcups.class.php

--- core/class/apcups.class.php
+++ core/class/apcups.class.php
@@ -271,11 +271,11 @@ class apcups extends eqLogic {
     foreach ($this->getCmd('info') as $cmd) {
       //$command = 	"apcaccess | grep TIMELEFT | awk '{print $3}'";
       if ($cmd->getLogicalId()=="model"){
-        $command = "/sbin/apcaccess status " . $apcupsd . " | grep " . $cmd->getLogicalId() . " | awk 'BEGIN {FS=\" : \"} {print $2}'";
+        $command = "/sbin/apcaccess status " . $apcupsd . " | grep " . strtoupper($cmd->getLogicalId()) . " | awk 'BEGIN {FS=\" : \"} {print $2}'";
       } elseif ($cmd->getLogicalId()=="outpower"){
         $command = "/sbin/apcaccess status " . $apcupsd . " | grep LOADPCT | awk '{print $3}'";
       } else {
-        $command = "/sbin/apcaccess status " . $apcupsd . " | grep " . $cmd->getLogicalId() . " | awk '{print $3}'";
+        $command = "/sbin/apcaccess status " . $apcupsd . " | grep " . strtoupper($cmd->getLogicalId()) . " | awk '{print $3}'";
       }
       $valeur = exec($command);
       if ($cmd->getLogicalId()=="outpower"){
