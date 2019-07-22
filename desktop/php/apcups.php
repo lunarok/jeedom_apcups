073ea7156d869414d87951d27674ceed5f07e35e
lunarok <lunarok@gmail.com>
2019-07-22 14:41:09 +0200

    Compliancy v4

--- desktop/php/apcups.php
+++ desktop/php/apcups.php
@@ -73,7 +73,7 @@ $eqLogics = eqLogic::byType('apcups');
                 <select class="form-control eqLogicAttr" data-l1key="object_id">
                   <option value="">{{Aucun}}</option>
                   <?php
-                  foreach (object::all() as $object) {
+                  foreach (jeeObject::all() as $object) {
                     echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
                   }
                   ?>
